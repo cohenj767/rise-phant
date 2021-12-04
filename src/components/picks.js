dbddb5b700799f941d967ecec5636c46f49723c0
Maximilian Roll <maxroll54@gmail.com>
2021-12-04 22:36:12 +0100

    Update phantoms

--- src/components/picks.js
+++ src/components/picks.js
@@ -2,19 +2,10 @@ export const picks = [
     {
         id: 1,
         owner: "x_Copy",
-        upgradeLvl: [715, 486, 500],
+        upgradeLvl: [1, 1, 1],
         isSharing: false,
         firstPrio: "None",
         secondPrio: "None",
-        location: "Shared Plot"
-    },
-    {
-        id: 2,
-        owner: "ChainArts",
-        upgradeLvl: [1000, 1000, 500],
-        isSharing: true,
-        firstPrio: "CoraSN",
-        secondPrio: "Qi_Venom",
         location: "Shared Plot"
     }
 ]
