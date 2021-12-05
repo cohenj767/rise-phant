7052d0ceed06af3e54b708fd91c10ae79da8e8fa
Maximilian Roll <maxroll54@gmail.com>
2021-12-05 02:40:56 +0100

    added unorth pick

--- src/components/picks.js
+++ src/components/picks.js
@@ -25,5 +25,14 @@ export const picks = [
         firstPrio: "CoraSN",
         secondPrio: "Qi_Venom",
         location: "ChainArts' Plot"
+    },
+    {
+        id: 4,
+        owner: "Unorthqdox",
+        upgradeLvl: [1, 1, 1],
+        isSharing: false,
+        firstPrio: "None",
+        secondPrio: "None",
+        location: "ChainArts' Plot"
     }
 ]
